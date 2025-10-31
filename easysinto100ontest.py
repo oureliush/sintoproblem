ba35a5de7388ed32f0d6de4cf1332c69451f2a5f
O <ourelius24@gmail.com>
2025-10-31 12:31:27 -0400

    init

---
+++ easysinto100ontest.py
@@ -0,0 +1,34 @@
+import math
+
+#The Deeping Wall has a height of 6.1 m.
+wall_height = 6.1 #meters
+
+#If Legolas is 2 m tall and shoots his bow from shoulder height (assume the shoulders are ⅘ of the body’s height)
+LegoLas_Height = 2.0 #meters
+Shoulder_Height = (4/5) * LegoLas_Height 
+
+#the Bow of the Galadhrim fires its arrows at an initial velocity of 89.4 m/s 
+init_v_arrow = 89.4 #m/s
+
+arrow_shot_height = Shoulder_Height + wall_height
+
+print(arrow_shot_height)
+
+#Assume a neck height of 1.9 m from the ground
+deadguysneckheight = 1.9 #meters
+
+#it states the son of whoeverthefrick is standing on top of the base and the base to the guy he wants to shoot is 360 meters sooooooooo ya
+frombasetodeadguy = 360 #meters
+
+#no air resistants because the arrows come from buttfuck nowhere
+
+
+#whatever the arrows height at max thing add arrow_shot_height variable to it
+
+
+
+
+#basically
+#
+
+math.radians
